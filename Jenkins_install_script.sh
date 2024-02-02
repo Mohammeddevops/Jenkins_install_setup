b22f1f0af7098be46aada2875be98518b5ca28d2
Mohammed <100871044+Mohammeddevops@users.noreply.github.com>
2024-02-02 23:52:38 +0530

    Update Jenkins_install_script.sh

--- Jenkins_install_script.sh
+++ Jenkins_install_script.sh
@@ -6,7 +6,7 @@ sudo apt update && sudo apt upgrade -y
 # 	Install Java (required for Jenkins)           #
 #######################################################
 sudo apt update
-sudo apt install openjdk-11-jre
+sudo apt install openjdk-11-jre -y
 java -version
 
 
@@ -19,7 +19,7 @@ sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
 
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > 
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install jenkins -y
